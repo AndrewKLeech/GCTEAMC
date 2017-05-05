0e24919dfb6af165eb8f8925b2663c4e71d51746
AndrewKLeech <andrewkennethleech@gmail.com>
2017-05-05 22:15:58 +0900

    connect addRoom to front end, bed type and smoking not working

--- src/com/example/service/MainUI.java
+++ src/com/example/service/MainUI.java
@@ -134,12 +134,12 @@ public class MainUI {
 	public String addRoom(HttpServletRequest request, HttpServletResponse repsonse){
 		Controller controller = new Controller();
 		String forwardToJsp = "";	
-		String roomNo = request.getParameter("roomNo");
+		String addRoomNumber = request.getParameter("addRoomNumber");
 		String bedType = request.getParameter("bedType");
 		String smoking = request.getParameter("smoking");
-		String price = request.getParameter("price");
-		controller.addRoom(roomNo,bedType,smoking,price);
-		forwardToJsp = "/homepage.html";
+		String addRoomPrice = request.getParameter("addRoomPrice");
+		controller.addRoom(addRoomNumber,bedType,smoking,addRoomPrice);
+		forwardToJsp = "/manageRoom.jsp";
 		return forwardToJsp;
 	}
 
